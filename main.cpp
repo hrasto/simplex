922f47527515d7f5ef096c83eb2dcf510692ef30
andradac96 <andrada.colceriu@yahoo.com>
2017-01-05 19:41:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void lpsolve(int n, double* c, int k, double** A, double* b){
     // b - rechte Seite
 
     // eine Tableau instance erstellen
-    // umformen, bis möglich
+    // umformen, bis mÃ¶glich
     // array mit resultaten retournieren
 
     Tableau t = Tableau(n,k,c,A,b);
@@ -32,7 +32,7 @@ int main()
     // Daten einlesen
     // Lpsolve Funktion aufrufen
 
-    int n = 2;
+   /* int n = 2;
     int k = 3;
 
     double* c = new double[n]; // zielfunk.
@@ -53,7 +53,55 @@ int main()
     double* b = new double[k];
     b[0] = 4;
     b[1] = 12;
-    b[2] = 18;
+    b[2] = 18;*/
+
+
+
+int x, i, j, n, k;
+
+    string filename;
+    cin >> filename;
+    ifstream f;
+    f.open(filename);
+
+
+    f >> x;
+    n = x;
+
+    f >> x;
+    k = x;
+    //cout << n << ' ' << k <<endl;
+    double* c = new double[n];
+    for( i = 0; i < n; i++)
+    {
+        f >> x;
+        c[i] = x;
+    }
+
+    for(int i = 0; i < n; i++)
+       cout << c[i] << ' ';
+    cout << endl;
+
+    double** A = new double*[k]; // nb
+    //double A[100][100];
+    double* b = new double[k];
+   //cout<< n << ' ' << k;
+
+    for(i = 0; i < k; i++)
+    {
+        A[i] = new double[n];
+        for(j = 0; j < n; j++)
+        {
+            f >> x;
+            A[i][j] = x;
+        }
+        if(j+1 == n+1)
+        {
+            f >> x;
+            b[i] = x;
+        }
+
+    }
 
     /* random werte:
 
